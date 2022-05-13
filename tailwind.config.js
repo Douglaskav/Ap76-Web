a5b6d40c8eea9d3afa27501826a9a657f5df583f
Douglaskav <dnmsmpvp@gmail.com>
2022-05-13 16:09:57 -0300

    package.json of tailwind and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        logo_color: {
+          400: "#616161",
+          500: "#333333",
+        },
+
+        primary: {
+          300: "#2AF580",
+          500: "#2ECC71",
+          700: "#44AB70",
+        },
+
+        secondary: {
+          300: "#FF902E",
+          500: "#E67E22",
+          700: "#C25F08",
+        },
+
+        desc: {
+          300: "#888888",
+          500: "#555555",
+          700: "#AAAAAA",
+        },
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+        body: ['"Rubik"']
+      },
+    },
   },
   plugins: [],
-}
+};
