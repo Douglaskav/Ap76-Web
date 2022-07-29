589fc3641cba2c65ffd0b901fd35ef16f420a63a
Douglaskav <dnmsmpvp@gmail.com>
2022-07-29 15:43:26 -0300

    ErrorAlert

--- src/pages/Login/components/Form/index.jsx
+++ src/pages/Login/components/Form/index.jsx
@@ -1,45 +1,52 @@
+import { useState } from "react";
 import { EyeSlash } from "phosphor-react";
+import { ErrorAlert } from "../../../../components/ErrorAlert/";
 import "./styles.css";
 
 export function Form() {
+  const [showErrorAlert, setShowErrorAlert] = useState(false);
+
   return (
     <div className="container-form">
-      <div>
-        <h2 className="form-container-title">Login</h2>
-        <form>
-          <div className="form-login-inputgroup">
-            <label className="form-login-label">Email</label>
+      {showErrorAlert ? (
+        <ErrorAlert setShowErrorAlert={setShowErrorAlert} />
+      ) : (
+        ""
+      )}
+      <h2 className="form-container-title mt-20">Login</h2>
+      <form>
+        <div className="form-login-inputgroup">
+          <label className="form-login-label">Email</label>
+          <input
+            type="email"
+            placeholder="example@placeholder.com"
+            className="form-login-input"
+          />
+        </div>
+
+        <div className="form-login-inputgroup mt-12">
+          <label className="form-login-label">Password</label>
+          <div className="flex items-center">
             <input
-              type="email"
-              placeholder="example@placeholder.com"
-              className="form-login-input"
+              type="password"
+              placeholder="***********"
+              className="form-login-input flex-1"
             />
+            <EyeSlash size={24} color="#aaa" />
           </div>
+        </div>
 
-          <div className="form-login-inputgroup mt-12">
-            <label className="form-login-label">Password</label>
-            <div className="flex items-center">
-              <input
-                type="password"
-                placeholder="***********"
-                className="form-login-input flex-1"
-              />
-              <EyeSlash size={24} color="#aaa" />
-            </div>
-          </div>
+        <div className="form-login-links-container">
+          <a href="/forgot" className="form-login-link">
+            I forgot my password
+          </a>
+          <a href="/register" className="form-login-link">
+            I don't have an account yet
+          </a>
+        </div>
 
-          <div className="form-login-links-container">
-            <a href="/forgot" className="form-login-link">
-              I forgot my password
-            </a>
-            <a href="/register" className="form-login-link">
-              I don't have an account yet
-            </a>
-          </div>
-
-          <button className="form-login-button">Login</button>
-        </form>
-      </div>
+        <button className="form-login-button">Login</button>
+      </form>
     </div>
   );
 }
--- src/pages/Register/components/Form/index.jsx
+++ src/pages/Register/components/Form/index.jsx
@@ -1,57 +1,84 @@
+import { useState } from "react";
 import { EyeSlash } from "phosphor-react";
+import api from "../../../../services/api";
 
 export function Form() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  async function handleSubmitForm(event) {
+    event.preventDefault();
+    
+    const response = await api.post("/user/create", formData);
+    console.log(response.data);
+  }
+
   return (
     <div className="container-form">
-      <div>
-        <h2 className="form-container-title">Register</h2>
-        <form>
-          <div className="form-login-inputgroup">
-            <label className="form-login-label">Username</label>
+      <h2 className="form-container-title mt-10">Register</h2>
+      <form>
+        <div className="form-login-inputgroup">
+          <label className="form-login-label">Username</label>
+          <input
+            type="name"
+            name="username"
+            placeholder="Lucas Gabriel"
+            className="form-login-input"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-login-inputgroup mt-12">
+          <label className="form-login-label">Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="example@placeholder.com"
+            className="form-login-input"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-login-inputgroup mt-12 ">
+          <label className="form-login-label">Password</label>
+          <div className="flex items-center">
             <input
-              type="name"
-              placeholder="Lucas Gabriel"
-              className="form-login-input"
+              type="password"
+              name="password"
+              placeholder="***********"
+              className="form-login-input flex-1"
+              onChange={handleInputChange}
             />
+            <EyeSlash size={24} color="#aaa" />
           </div>
-          <div className="form-login-inputgroup mt-12">
-            <label className="form-login-label">Email</label>
+        </div>
+        <div className="form-login-inputgroup mt-12 ">
+          <label className="form-login-label">Repeat Password</label>
+          <div className="flex items-center">
             <input
-              type="email"
-              placeholder="example@placeholder.com"
-              className="form-login-input"
+              type="password"
+              placeholder="***********"
+              className="form-login-input flex-1"
             />
+            <EyeSlash size={24} color="#aaa" />
           </div>
-          <div className="form-login-inputgroup mt-12 ">
-            <label className="form-login-label">Password</label>
-            <div className="flex items-center">
-              <input
-                type="password"
-                placeholder="***********"
-                className="form-login-input flex-1"
-              />
-              <EyeSlash size={24} color="#aaa" />
-            </div>
-          </div>
-          <div className="form-login-inputgroup mt-12 ">
-            <label className="form-login-label">Repeat Password</label>
-            <div className="flex items-center">
-              <input
-                type="password"
-                placeholder="***********"
-                className="form-login-input flex-1"
-              />
-              <EyeSlash size={24} color="#aaa" />
-            </div>
-          </div>
-          <div className="form-login-links-container">
-            <a href="/login" className="form-login-link">
-              I already have an account
-            </a>
-          </div>
-          <button className="form-login-button">Register</button>
-        </form>
-      </div>
+        </div>
+        <div className="form-login-links-container">
+          <a href="/login" className="form-login-link">
+            I already have an account
+          </a>
+        </div>
+        <button className="form-login-button" onClick={handleSubmitForm}>
+          Register
+        </button>
+      </form>
     </div>
   );
 }
