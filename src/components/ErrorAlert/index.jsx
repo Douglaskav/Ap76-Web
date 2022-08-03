885970e51506715017199a593f2085b643b9439f
Douglaskav <dnmsmpvp@gmail.com>
2022-08-03 10:07:02 -0300

    ConfirmOTPPage

--- src/components/ErrorAlert/index.jsx
+++ src/components/ErrorAlert/index.jsx
@@ -1,16 +1,16 @@
 import { Warning, XCircle } from "phosphor-react";
 
-export function ErrorAlert({ setShowErrorAlert }) {
+export function ErrorAlert({ setShowErrorAlert, message }) {
 	return (
 		<div className="p-4 bg-[#f8d7da] border-1 border-[#f5c6cb] rounded flex items-center justify-between">
 			<span className="text-[#721c24] flex items-center">
 				<Warning size={24} className="mr-4" />
-				Already exists an user with the email provided.
+				{ message}
 			</span>
 			<XCircle
 				size={24}
 				className="text-[#721c24] cursor-pointer"
-				onClick={() => setShowErrorAlert(false)}
+				onClick={() => setShowErrorAlert({ show: false, message: ""})}
 			/>
 		</div>
 	);
